cd48711120dbfa4b34ac05e03ec726ae83a5f670
Jan Behrens <dev@jpbehrens.de>
2022-01-12 18:10:48 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,32 @@
   <div class="bg-gray-100 text-dark max-w-2xl rounded-md mx-5 md:mx-auto my-5">
     <h1 class="py-2 text-2xl text-center">Meine Todoliste</h1>
     <h2 class="pb-2 text-1xl text-gray-500 text-center">
-      Noch offen: {{ todos.length }}
+      Noch offen: {{ todos.filter((todo) => !todo.done).length }}
     </h2>
     <div class="w-full bg-gray-200 flex justify-center p-1">
-      <input type="text" class="grow w-full px-3 mr-1" />
-      <button @click="demo" class="bg-green-600 text-white py-1 px-3">
+      <input type="text" class="grow w-full px-3 mr-1" v-model="newTodo" />
+      <button @click="addTodo" class="bg-green-600 text-white py-1 px-3">
         Anlegen
       </button>
     </div>
-    <Todo message="Hallo" />
+
+    <!-- Scheifen in VUE -->
+    <div v-for="(todo, index) in todos" :key="index">
+      <Todo
+        :todoProps="todo"
+        :todoIndex="index"
+        @toggel-done="toggleDone"
+        @remove-todo="removeTodo"
+      />
+    </div>
+
+    <!-- Binding
+    <Todo :todoProps="todos[0].label" />
+    <Todo :todoProps="todos[1].label" />
+    <Todo :todoProps="todos[2].label" />
+
+    Übergabe ohne Binding
+    <Todo todoProps="NOT BINDED PROP" /> -->
   </div>
 </template>
 
@@ -23,25 +40,38 @@ export default {
   },
   data() {
     return {
+      newTodo: "",
       todos: [
         {
           label: "Was machen",
-          done: 0,
+          done: false,
         },
         {
           label: "Etwas lachen",
-          done: 0,
+          done: true,
         },
         {
           label: "Lass krachen",
-          done: 0,
+          done: false,
         },
       ],
     };
   },
   methods: {
-    demo() {
-      alert(this.input);
+    addTodo() {
+      if(this.newTodo.trim() === '') return;
+      this.todos.push({
+        label: this.newTodo,
+        done: false,
+      });
+
+      this.newTodo = "";
+    },
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
     },
   },
 };
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,28 +1,38 @@
 <template>
-  <h1>{{message.toUpperCase()}}</h1>
-  <span>{{count}}</span>
-  <button v-on:click="countUp">Hochzählen</button>
-  <button @click="countDown">Runterzählen</button>
+  <div
+    class="flex justify-between items-center bg-white p-2 border border-indigo-200 m-2"
+    :class="{ 'bg-green-600': todo.done, 'bg-red-600': !todo.done }"
+  >
+    <h1 @click="toggleTodo" class="cursor-pointer">{{ todo.label }}</h1>
+    <button
+      @click="removeTodo"
+      class="bg-indigo-500 text-xs text-white py-1 px-3 rounded-sm"
+    >
+      Löschen
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Todo',
-  data(){
+  name: "Todo",
+  props: ["todoProps", "todoIndex"],
+  data() {
     return {
-      message: "ich bin eine Message!",
-      count: 0
-    }
+      todo: this.todoProps,
+    };
   },
-  methods:{
-    countUp(){
-      this.count++
+  methods: {
+    toggleTodo() {
+      // EMIT SCHICKZ EINEN BESTIMMTEN WERT AN DIE ÜBERGEORDNETE KOMPONENTE
+      this.$emit("toggel-done", this.todoIndex);
     },
-    countDown(){
-      this.count--
-    }
-  }
-}
+    removeTodo() {
+      // EMIT SCHICKZ EINEN BESTIMMTEN WERT AN DIE ÜBERGEORDNETE KOMPONENTE
+      this.$emit("remove-todo", this.todoIndex);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

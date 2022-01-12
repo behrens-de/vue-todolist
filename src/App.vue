61442b6d0963cb3b52cb2ec804bb52ee31800e57
Jan Behrens <dev@jpbehrens.de>
2022-01-12 12:05:30 +0100

    updated: component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloVue/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloVue from './components/HelloVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloVue
   }
 }
 </script>
